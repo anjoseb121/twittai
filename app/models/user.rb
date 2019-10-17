fc0339cf4cff89b53d1dbee0fb741b5e359c4f8b
Antonio Barandica <anjose.121@gmail.com>
2019-10-17 10:31:58 -0500

    Add tweets

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,6 @@ class User < ApplicationRecord
 
   acts_as_followable
   acts_as_follower
+
+  has_many :tweets
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_10_17_151718) do
+ActiveRecord::Schema.define(version: 2019_10_17_152612) do
 
   create_table "follows", force: :cascade do |t|
     t.string "followable_type", null: false
@@ -24,6 +24,14 @@ ActiveRecord::Schema.define(version: 2019_10_17_151718) do
     t.index ["followable_type", "followable_id"], name: "index_follows_on_followable_type_and_followable_id"
     t.index ["follower_id", "follower_type"], name: "fk_follows"
     t.index ["follower_type", "follower_id"], name: "index_follows_on_follower_type_and_follower_id"
+  end
+
+  create_table "tweets", force: :cascade do |t|
+    t.string "content"
+    t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_tweets_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
